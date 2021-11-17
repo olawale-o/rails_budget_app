b501937dcdace517e6756c156414f7c9cf46dbe0
olawale-o <omoogunolawale@gmail.com>
2021-11-17 11:14:55 +0100

    Add associations and validations to treaty model

---
+++ app/models/treaty.rb
@@ -0,0 +1,6 @@
+class Treaty < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id, class_name: 'User'
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  has_many_and_belongs_to :groups
+end

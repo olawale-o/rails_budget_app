6ce7f1149ee84cdf9b545de0e3790ada503f5ad8
olawale-o <omoogunolawale@gmail.com>
2021-11-17 11:16:14 +0100

    Modify association name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
-  has_many_and_belongs_to :treaties
+  has_and_belongs_to_many :treaties
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :icon, presence: true, uniqueness: true
 end
--- app/models/treaty.rb
+++ app/models/treaty.rb
@@ -2,5 +2,5 @@ class Treaty < ApplicationRecord
   belongs_to :user, foreign_key: :author_id, class_name: 'User'
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  has_many_and_belongs_to :groups
+  has_and_belongs_to_many :groups
 end

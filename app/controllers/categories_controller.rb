55bceca0fd462373b1b2287da7364e0b663c9674
olawale-o <omoogunolawale@gmail.com>
2021-11-18 14:07:01 +0100

    User can add new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,26 @@ class CategoriesController < ApplicationController
   def index
     @groups = current_user.groups.includes(:treaties)
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(category_params)
+    current_user.groups << @group
+    if @group.save
+      flash[:notice] = 'Category created successfully'
+      redirect_to categories_path
+    else
+      flash[:alert] = 'Something went wrong'
+      render :new
+    end
+  end
+
+  private 
+  
+  def category_params
+    params.require(:group).permit(:name, :icon)
+  end
 end

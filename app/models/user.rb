e3476d49c7d1f5ea661b5abc0d01fd827fbba184
olawale-o <omoogunolawale@gmail.com>
2021-11-19 09:16:32 +0100

    Define my_categories_and_treaties instance method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ApplicationRecord
          :confirmable
   has_many :categories
   has_many :treaties
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+
+  before_validation :remove_whitespace
+
+  def my_categories_and_treaties
+    categories.includes(:treaties)
+  end
+
+  private
+
+  def remove_whitespace
+    self.email = email.strip if email.nil?
+    self.name = name.strip if name.nil?
+  end
 end

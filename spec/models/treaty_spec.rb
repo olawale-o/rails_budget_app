dd516a43bb4baf294d5aa86ec53edc9349592afa
olawale-o <omoogunolawale@gmail.com>
2021-11-19 08:56:40 +0100

    Create model specs

---
+++ spec/models/treaty_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Treaty, type: :model do
+  describe 'validations' do
+    let(:treaty) { create(:treaty) }
+    it 'name should be present' do
+      treaty.name = nil
+      expect(treaty).to_not be_valid
+    end
+
+    it 'amount should be present' do
+      treaty.amount = nil
+      expect(treaty).to_not be_valid
+    end
+
+    it 'owner should be present' do
+      treaty.user = nil
+      expect(treaty).to_not be_valid
+    end
+  end
+end

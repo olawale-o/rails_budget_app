95a5dedfd4aad9428cc2952706c22f2e8b851b47
olawale-o <omoogunolawale@gmail.com>
2021-11-19 09:17:43 +0100

    Define treaties_with_user and total_amount instance methods in category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,21 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :treaties
   validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
   validates :icon, presence: true, uniqueness: true
+
+  before_validation :remove_whitespace
+
+  def treaties_with_user
+    treaties.includes(:user)
+  end
+
+  def total_amount
+    treaties.sum(:amount)
+  end
+
+  private
+
+  def remove_whitespace
+    self.name = name.strip if name.nil?
+    self.icon = icon.strip if icon.nil?
+  end
 end

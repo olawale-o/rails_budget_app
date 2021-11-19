0cc4d1aff0b8087a19ccad3f27795767c1a9a80b
olawale-o <omoogunolawale@gmail.com>
2021-11-19 09:33:20 +0100

    Modify categories view and controller

--- app/models/group.rb
+++
@@ -1,6 +0,0 @@
-class Group < ApplicationRecord
-  belongs_to :user
-  has_and_belongs_to_many :treaties
-  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
-  validates :icon, presence: true, uniqueness: true
-end

b00dd11a7c04387dd7cca502c9f8dc23fad3747c
olawale-o <omoogunolawale@gmail.com>
2021-11-19 08:42:00 +0100

    Add factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,20 @@
+FactoryBot.define do
+  sequence(:email) { |n| "email#{n}@test.com" }
+  factory :user do
+    name { 'name' }
+    email
+    password { 'password' }
+    password_confirmation { 'password' }
+    confirmed_at { Date.today }
+
+    factory :with_categories do
+      transient do
+        categories_count { 3 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:category, evaluator.categories_count, user: user)
+      end
+    end
+  end
+end

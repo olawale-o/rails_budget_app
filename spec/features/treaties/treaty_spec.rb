88a4f59db476d5d3b39c22cdcca2743faf3d91b4
olawale-o <omoogunolawale@gmail.com>
2021-11-21 17:37:26 +0100

    Split test

--- spec/features/treaties/treaty_spec.rb
+++ spec/features/treaties/treaty_spec.rb
@@ -21,17 +21,5 @@ RSpec.feature 'Treaty New Page', type: :feature do
       expect(current_path).to eq category_path(@food.id)
       expect(page.has_content?('Treaty created successfully')).to be_truthy
     end
-    scenario 'with invalid credentials' do
-      within 'form' do
-        fill_in 'treaty[name]', with: ''
-        fill_in 'treaty[amount]', with: ''
-        select '', from: 'treaty_category_ids', match: :first
-      end
-      click_button 'Add'
-      expect(current_path).to eq treaties_path
-      expect(page).to have_content("Name can't be blank")
-      expect(page).to have_content("Amount can't be blank")
-      expect(page).to have_content('Amount is not a number')
-    end
   end
 end

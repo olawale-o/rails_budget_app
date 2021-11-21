f4f3764bc4a1738d7bdccb697b639537cb97c8b8
olawale-o <omoogunolawale@gmail.com>
2021-11-21 15:00:56 +0100

    Add more validations to treaty model

--- app/models/treaty.rb
+++ app/models/treaty.rb
@@ -3,5 +3,6 @@ class Treaty < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :author_id, message: 'already taken by you' },
                    length: { maximum: 50 }
   validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :categories, presence: true
   has_and_belongs_to_many :categories
 end

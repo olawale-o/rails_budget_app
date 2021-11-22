6e97c8b99f5452e593a462ca1816ae20571a0349
olawale-o <omoogunolawale@gmail.com>
2021-11-22 10:27:23 +0100

    Add more feature specs

--- spec/features/categories/user_login_with_category_spec.rb
+++ spec/features/categories/user_login_with_category_spec.rb
@@ -13,15 +13,22 @@ RSpec.feature 'Category Index Page', type: :feature do
   describe 'Category Index Page' do
     context 'when user is logged in' do
       scenario 'with categories i should see list of categories' do
+        all_images = all('img')
+        expect(all_images.size).to eq(@user.categories.count)
         @user.categories.each do |category|
           expect(page).to have_content(category.name)
           expect(page).to have_content("$#{category.total_amount}")
         end
+        all_images.each_with_index { |_val, i| expect(all_images[i][:src]).to eq(@user.categories[i].icon) }
       end
-      it 'should see add new link' do
+      it 'i should see add new link' do
         expect(find_link('ADD A NEW CATEGORY').visible?).to be true
         click_link('ADD A NEW CATEGORY')
         expect(page.current_path).to eq(new_category_path)
+      end
+      it 'i should go to selected category detail page' do
+        click_link("category#{@user.categories.last.id}")
+        expect(page.current_path).to eq(category_path(@user.categories.last))
       end
     end
   end
